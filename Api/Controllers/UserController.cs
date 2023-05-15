a5206afe3ccb9d870090fe851ab6cc33a97156b5
Wiliam GM <gomesruivo@gmail.com>
2023-05-15 12:08:47 -0300

    Api Project Added

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post() 
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+        
+    }
+}
